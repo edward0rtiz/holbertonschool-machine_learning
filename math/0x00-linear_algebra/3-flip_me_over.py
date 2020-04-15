8584600480b3ea69addf298403e79b8abc9e13ce
edward0rtiz <931@holbertonschool.com>
2020-04-15 14:09:10 -0500

    [fix] add docstring

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -3,6 +3,17 @@
 
 
 def matrix_transpose(matrix):
-    """ return a new matrix transposed """
-    new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    """ return a new matrix transposed
+
+    Args:
+        matrix: given list
+
+    Return:
+        new_matrix: Transposed matrix
+
+    """
+    if type(matrix[0]) != list:
+        return [len(matrix)]
+    else:
+        new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
     return new_matrix
