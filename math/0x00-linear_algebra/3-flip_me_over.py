e464a20716d1292ce8514399f74fc3ca89f26775
edward0rtiz <931@holbertonschool.com>
2020-04-15 14:05:43 -0500

    [fix] remove condition that check if matris is list

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -4,8 +4,5 @@
 
 def matrix_transpose(matrix):
     """ return a new matrix transposed """
-    if type(matrix[0]) != list:
-        return [len(matrix)]
-    else:
-        new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
     return new_matrix
