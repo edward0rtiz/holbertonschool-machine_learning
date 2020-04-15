7e65afb7e58f9bc1f5bda692feaff49df2bd8404
edward0rtiz <931@holbertonschool.com>
2020-04-15 14:26:05 -0500

    [fix] ln 18 line too long

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -15,5 +15,6 @@ def matrix_transpose(matrix):
     if type(matrix[0]) != list:
         return [len(matrix)]
     else:
-        new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+        new_matrix = [[matrix[j][i] for j in range(len(matrix))]
+                      for i in range(len(matrix[0]))]
     return new_matrix
