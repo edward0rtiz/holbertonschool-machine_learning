be929ed4146fec7d916e9c2928a81c180c73a63c
edward0rtiz <931@holbertonschool.com>
2020-04-15 23:28:32 -0500

    [feature] add transpose function using numpy

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function to transpose a matrix using numpy"""
+
+
+def np_transpose(matrix):
+    """ transpose of a matrix
+
+    Args:
+        matrix: Given matrix
+
+    Return:
+        transposed matrix
+    """
+    return matrix.transpose()
