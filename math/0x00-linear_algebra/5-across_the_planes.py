1787dfcfc8df3d79eebe06554965a3e6b9910536
edward0rtiz <931@holbertonschool.com>
2020-04-15 20:11:00 -0500

    [fix] add contition of len mat1 and len mat2 at begining

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -14,11 +14,10 @@ def add_matrices2D(mat1, mat2):
     """
     if len(mat1) != len(mat2):
         return None
+    elif len(mat1[0]) != len(mat2[0]):
+        return None
     else:
         new_mat = [[mat1[i][j] + mat2[i][j]
                     for j in range(len(mat1[0]))] for i in range(len(mat1))]
-        if len(mat1[0]) != len(mat2[0]):
-            return None
-        else:
-            for count in new_mat:
-                return new_mat
+        for count in new_mat:
+            return new_mat
