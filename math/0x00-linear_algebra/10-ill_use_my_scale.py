cb1255dbc1568ae844f03882e60ec93682fa746f
edward0rtiz <931@holbertonschool.com>
2020-04-15 23:12:52 -0500

    [feature] shape of an array

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function to calculate shape of an array using numpy"""
+
+
+def np_shape(matrix):
+    """ shape of an array
+
+    Args:
+        matrix: Given matrix
+
+    Return:
+        shape of an array
+    """
+    return matrix.shape
