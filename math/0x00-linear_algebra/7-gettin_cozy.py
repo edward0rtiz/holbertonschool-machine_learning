045a072e6c81551206d77146f6c190c4ae974ce9
edward0rtiz <931@holbertonschool.com>
2020-04-15 21:27:50 -0500

    [fix] ad axis rule

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -16,12 +16,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
 
     """
 
-    if (len(mat1[0]) == len(mat2[0])):
+    if (len(mat1[0]) == len(mat2[0])) and axis == 0:
         new_mat1 = [x[:] for x in mat1]
         new_mat2 = [x[:] for x in mat2]
         new_mat = new_mat1 + new_mat2
         return new_mat
-    elif (len(mat1) == len(mat2)):
+    elif (len(mat1) == len(mat2)) and axis == 1:
         new_mat = [mat1[x] + mat2[x] for x in range(len(mat1))]
         return new_mat
     else:
