7c8ca32eedaf6bbd909a922e0bd5922b27c91a0e
edward0rtiz <931@holbertonschool.com>
2020-04-20 17:15:44 -0500

    [feature] add exponential decay of radioactve elements

---
+++ math/0x01-plotting/3-two.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 21000, 1000)
+r = np.log(0.5)
+t1 = 5730
+t2 = 1600
+y1 = np.exp((r / t1) * x)
+y2 = np.exp((r / t2) * x)
+
+# your code here
+plt.xlabel('Time (years)')
+plt.ylabel('Fraction Remaining')
+plt.title("Exponential Decay of Radioactive Elements")
+plt.xlim(0, 20000)
+plt.ylim(0, 1)
+plt.plot(x, y1, c='red', linestyle='dashed', label='C-14')
+plt.plot(x, y2, c='green', label='Ra-226')
+plt.legend()
+plt.show()
