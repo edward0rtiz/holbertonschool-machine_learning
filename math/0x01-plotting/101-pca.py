b8197f691433632a4c8d1a3fbb7f192a66e6c29c
edward0rtiz <931@holbertonschool.com>
2020-04-20 22:51:42 -0500

    [feature] 102 axes3d map

---
+++ math/0x01-plotting/101-pca.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+lib = np.load("pca.npz")
+data = lib["data"]
+labels = lib["labels"]
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+pca_data = np.matmul(norm_data, Vh[:3].T)
+
+# your code here
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.title("PCA of Iris Dataset")
+ax.set_xlabel("U1")
+ax.set_ylabel("U2")
+ax.set_zlabel("U3")
+ax.scatter(xs=pca_data[:, 0], ys=pca_data[:, 1], zs=pca_data[:, 2],
+           c=labels, cmap=plt.cm.plasma)
+plt.show()
+
+
