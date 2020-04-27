7930b4978a81cf5f969643457f04e2dc87d1207e
edward0rtiz <931@holbertonschool.com>
2020-04-26 19:47:10 -0500

    [update] function to derivative a poly

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""function that derivative a polynomial f(x)"""
+
+
+def poly_derivative(poly):
+    """function to calculate a derivative of a poly"""
+
+    if type(poly) is not list:
+        return None
+    elif not poly:
+        return None
+    else:
+        power = [i for i in range(len(poly))]
+        df = [power[x] * poly[x] for x in range(1, len(poly))]
+        if len(list(df)) == 1:
+            return [0]
+        else:
+            return df
