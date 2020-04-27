ebed7f0ae8ffa04516d06dfbc6e09e1076610070
edward0rtiz <931@holbertonschool.com>
2020-04-26 23:47:00 -0500

    [fix] ln 25 casting x to int to reduce decimals

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -13,7 +13,7 @@ def poly_integral(poly, C=0):
         return None
     elif len(poly) == 1:
         return [0]
-    elif type(C)is not int:
+    elif type(C) is not int:
         return None
     elif poly == [0]:
         return [C]
@@ -21,6 +21,6 @@ def poly_integral(poly, C=0):
         integral = []
         integral.append(C)
         for i in range(len(poly)):
-            x = poly[i]/(i + 1)
-            integral.append(x if x.is_integer() else x)
+            x = poly[i] / (i + 1)
+            integral.append(int(x) if x.is_integer() else x)
         return integral
