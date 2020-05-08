7c882c980dc8cc006f51c685e12b6891282425f1
edward0rtiz <931@holbertonschool.com>
2020-05-08 18:56:30 -0500

    [fix] ln 42 self b inserted at ln 35

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -32,6 +32,7 @@ class DeepNeuralNetwork():
         for lay in range(self.L):
             if layers[lay] < 0 or type(layers[lay]) is not int:
                 raise TypeError("layers must be a list of positive integers")
+            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
             if lay == 0:
                 He_val = np.random.randn(layers[lay], nx) * np.sqrt(2 / nx)
                 self.weights["W" + str(lay + 1)] = He_val
@@ -39,4 +40,3 @@ class DeepNeuralNetwork():
                 He_val1 = np.random.randn(
                     layers[lay], layers[lay - 1]) * np.sqrt(2 / nx)
                 self.weights["W" + str(lay + 1)] = He_val1
-            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
