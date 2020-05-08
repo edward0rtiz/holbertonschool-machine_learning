94f3c4737775334d11de25df2f1dd46b154b8e2a
edward0rtiz <931@holbertonschool.com>
2020-05-08 18:46:42 -0500

    [feature] public classs deep neural network

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -5,8 +5,17 @@ import numpy as np
 
 
 class DeepNeuralNetwork():
+    """
+    Class Deep Neural Network
+    """
 
     def __init__(self, nx, layers):
+        """
+
+        Args:
+            nx: input value
+            nodes: nodes placed in the hidden layer
+        """
 
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -20,10 +29,14 @@ class DeepNeuralNetwork():
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        for i in range(self.L):
-            if layers[i] < 0 or type(layers[i]) is not int:
+        for lay in range(self.L):
+            if layers[lay] < 0 or type(layers[lay]) is not int:
                 raise TypeError("layers must be a list of positive integers")
-            if i == 0:
-                He_val = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
-                self.weights["W" + str(i + 1)] = He_val
-
+            if lay == 0:
+                He_val = np.random.randn(layers[lay], nx) * np.sqrt(2 / nx)
+                self.weights["W" + str(lay + 1)] = He_val
+            if lay > 0:
+                He_val1 = np.random.randn(
+                    layers[lay], layers[lay - 1]) * np.sqrt(2 / nx)
+                self.weights["W" + str(lay + 1)] = He_val1
+            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
