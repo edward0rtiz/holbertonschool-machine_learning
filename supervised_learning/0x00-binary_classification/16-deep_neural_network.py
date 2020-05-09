dee25a6ca838b77b09921b84632e1c0045bef1cf
edward0rtiz <931@holbertonschool.com>
2020-05-08 19:07:07 -0500

    [fix] line too long at

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -37,6 +37,7 @@ class DeepNeuralNetwork():
                 He_val = np.random.randn(layers[lay], nx) * np.sqrt(2 / nx)
                 self.weights["W" + str(lay + 1)] = He_val
             if lay > 0:
-                He_val = np.random.randn(
-                    layers[lay], layers[lay - 1]) * np.sqrt(2 / layers[lay - 1])
-                self.weights["W" + str(lay + 1)] = He_val
+                He_val1 = np.random.randn(layers[lay], layers[lay - 1])
+                He_val2 = np.sqrt(2 / layers[lay - 1])
+                He_val3 = He_val1 * He_val2
+                self.weights["W" + str(lay + 1)] = He_val3
