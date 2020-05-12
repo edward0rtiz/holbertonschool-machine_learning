63a4dce62624544ac99b4dca735a9e26f27a9431
edward0rtiz <931@holbertonschool.com>
2020-05-12 11:46:17 -0500

    [fix] remove dtype=int

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -18,6 +18,6 @@ def one_hot_encode(Y, classes):
     if Y.size is 0:
         return None
     else:
-        ohe = np.zeros((classes, Y.shape[0]), dtype=int)
+        ohe = np.zeros((classes, Y.shape[0]))
         ohe[Y, np.arange(Y.size)] = 1
         return ohe
