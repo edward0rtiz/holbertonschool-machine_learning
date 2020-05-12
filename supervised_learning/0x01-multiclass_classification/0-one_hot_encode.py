d86f973d4b8835f0ba3a72b5d7327a6e80947073
edward0rtiz <931@holbertonschool.com>
2020-05-12 11:52:18 -0500

    [fix] add case clsses not int

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Script to converts a numeric vector into a one-hot matrix"""
 
 
@@ -16,6 +16,8 @@ def one_hot_encode(Y, classes):
 
     """
     if Y.size is 0:
+        return None
+    elif type(classes) is not int:
         return None
     else:
         ohe = np.zeros((classes, Y.shape[0]))
