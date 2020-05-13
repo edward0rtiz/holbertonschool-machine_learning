6d0c48292bce38c0b1bfb1424a59c2d18f1b19f9
edward0rtiz <931@holbertonschool.com>
2020-05-12 22:37:25 -0500

    [fix] add case one_hot == 0 | == 1

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -22,4 +22,6 @@ def one_hot_decode(one_hot):
         return None
     elif len(one_hot.shape) != 2:
         return None
+    elif not np.all((one_hot == 0) | (one_hot == 1)):
+        return None
     return np.argmax(one_hot, axis=0)
