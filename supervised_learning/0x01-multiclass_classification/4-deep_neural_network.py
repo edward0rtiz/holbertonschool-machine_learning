f91a1b2bd9e65891658c066ab4a77fe6ac9662ef
edward0rtiz <931@holbertonschool.com>
2020-05-12 23:42:23 -0500

    [fix] ln 32 double quote

--- supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
@@ -29,7 +29,7 @@ class DeepNeuralNetwork():
             raise TypeError("layers must be a list of positive integers")
         if len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if activation != "sig" and activation != "tanh":
+        if activation != 'sig' and activation != 'tanh':
             raise ValueError("activation must be 'sig or 'tanh'")
 
         self.__L = len(layers)
