e638b486da75a41303f6e3692d0f529290df3b79
edward0rtiz <931@holbertonschool.com>
2020-05-15 00:38:24 -0500

    [feature] create layer

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Script to create a layer"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode='FAN_AVG'  # Average number of inputs and output connections.
+    )
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=initializer,
+                            name='layer')
+    return layer(prev)
