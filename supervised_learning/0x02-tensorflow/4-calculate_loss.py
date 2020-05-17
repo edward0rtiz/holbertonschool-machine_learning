342a391132975cf80baea0806301d49a4df39256
edward0rtiz <931@holbertonschool.com>
2020-05-16 23:08:31 -0500

    [update] docstring

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -5,7 +5,15 @@ import tensorflow as tf
 
 
 def calculate_loss(y, y_pred):
+    """
+    Method to calculate the cross-entropy loss
+    of a prediction
+    Args:
+        y: input data type label in a placeholder
+        y_pred: type tensor that contains the DNN prediction
 
+    Returns:
+
+    """
     loss_f = tf.losses.softmax_cross_entropy(y, y_pred)
     return loss_f
-
