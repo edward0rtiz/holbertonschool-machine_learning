a53ae69eb01102c63ec54e5d670484f176703e58
edward0rtiz <931@holbertonschool.com>
2020-05-16 23:11:05 -0500

    [update] docstring

--- supervised_learning/0x02-tensorflow/5-create_train_op.py
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -7,6 +7,14 @@ import tensorflow as tf
 
 
 def create_train_op(loss, alpha):
+    """
+    method to train a model in a DNN with tf.
+    Args:
+        loss: cross-entropy loss function
+        alpha: learning rate
 
+    Returns: Trained operation of de DNN using gradient descent
+
+    """
     opt = tf.train.GradientDescentOptimizer(alpha).minimize(loss)
     return opt
