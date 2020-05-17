39656e71ed48ee02c22d97107eaef8ebb1c03ba5
edward0rtiz <931@holbertonschool.com>
2020-05-17 17:11:50 -0500

    [get collection to retrieve as a list 1st index add [0]

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -15,14 +15,15 @@ def evaluate(X, Y, save_path):
     Returns: Evaluated model
 
     """
-    with tf.Session as sess:
+    with tf.Session() as sess:
         saver = tf.train.import_meta_graph("{}.meta".format(save_path))
         saver.restore(sess, save_path)
-        x = tf.get_collection("x")
-        y = tf.get_collection("y")
-        y_pred = tf.get_collection("y_pred")
-        accuracy = tf.get_collection("accuracy")
-        loss = tf.get_collection("loss")
+        #  tf.get_collection() returns a list. Retrieve only the 1st one
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
 
         eval_y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
         eval_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
