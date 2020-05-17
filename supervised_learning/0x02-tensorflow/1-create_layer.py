a5f4cc0a54e9723e711157c3faaa5f56e0439e80
edward0rtiz <931@holbertonschool.com>
2020-05-16 22:51:49 -0500

    [update] docstring

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -5,7 +5,16 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
+    """
 
+    Args:
+        prev: tensor of the previous layer
+        n: n nodes created
+        activation: activation function
+
+    Returns: Layer created with shape n
+
+    """
     initializer = tf.contrib.layers.variance_scaling_initializer(
         mode='FAN_AVG'  # Average number of inputs and output connections.
     )
