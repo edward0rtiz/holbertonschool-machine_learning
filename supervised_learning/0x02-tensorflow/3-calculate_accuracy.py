71d4f59e44a85ed670b0a0725110e12962b7bc1b
edward0rtiz <931@holbertonschool.com>
2020-05-16 23:05:11 -0500

    [update] docstring

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,8 +5,17 @@ import tensorflow as tf
 
 
 def calculate_accuracy(y, y_pred):
+    """
+    method to calculate the accuracy of a prediction in a DNN
+    Args:
+        y: input data type label in a placeholder
+        y_pred: type tenser that contains the DNN prediction
 
-    correct_prediction = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_pred, axis=1))
+    Returns: Prediction accuracy
+
+    """
+    correct_prediction = tf.equal(tf.argmax(y, axis=1),
+                                  tf.argmax(y_pred, axis=1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     return accuracy
