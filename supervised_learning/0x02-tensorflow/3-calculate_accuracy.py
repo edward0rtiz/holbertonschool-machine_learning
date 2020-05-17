bda6993b925dcdc9984b4a741e9b7294d9ef9be4
edward0rtiz <931@holbertonschool.com>
2020-05-16 23:28:15 -0500

    [fix] remove axis label

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -14,8 +14,8 @@ def calculate_accuracy(y, y_pred):
     Returns: Prediction accuracy
 
     """
-    correct_prediction = tf.equal(tf.argmax(y, axis=1),
-                                  tf.argmax(y_pred, axis=1))
+    correct_prediction = tf.equal(tf.argmax(y, 1),
+                                  tf.argmax(y_pred, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     return accuracy
