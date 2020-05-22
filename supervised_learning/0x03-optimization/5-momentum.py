76698f812f0808b1028c3e9f3c8156e751cc27bc
edward0rtiz <931@holbertonschool.com>
2020-05-22 15:59:28 -0500

    [fix] add docstring

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -3,7 +3,18 @@
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Function to calculate momentum optimization
+    Args:
+        alpha: hyper-parameter learning rate
+        beta1: momentum weight
+        var: numpy.ndarray containing the variable to be updated
+        grad: numpy.ndarray containing the gradient of var
+        v: previous first moment of var
 
+    Returns:  the updated variable and the new moment, respectively
+
+    """
     V = (beta1 * v) + ((1 - beta1) * grad)
     W = var - (alpha * V)
     return W, V
