edc328df5a6c3980b2fe0ca057a82db925641fa4
edward0rtiz <931@holbertonschool.com>
2020-05-23 20:47:35 -0500

    [fix] add activation and remove nme none

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -11,13 +11,14 @@ def create_batch_norm_layer(prev, n, activation):
     Args:
         prev: the activated output of the previous layer
         n: number of nodes in the layer to be created
-        activation: activation function that should be used on the output of the layer
+        activation: activation function that should be used
+                    on the output of the layer
 
     Returns: tensor of the activated output for the layer
 
     """
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    x = tf.layers.Dense(units=n, kernel_initializer=initializer)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    x = tf.layers.Dense(units=n, activation=None, kernel_initializer=init)
     x_prev = x(prev)
     scale = tf.get_variable('gamma', shape=[n])
     mean, variance = tf.nn.moments(x_prev, axes=[0])
@@ -31,6 +32,5 @@ def create_batch_norm_layer(prev, n, activation):
         offset,
         scale,
         variance_epsilon,
-        name=None
     )
     return activation(normalization)
