c1579b213312efc6db3066c79459b15276c61589
edward0rtiz <931@holbertonschool.com>
2020-05-23 21:11:58 -0500

    [update] model WIP

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -140,3 +140,7 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     return LRD
 
 
+def model(Data_train, Data_valid, layers, activations, alpha=0.001,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, decay_rate=1,
+          batch_size=32, epochs=5, save_path='/tmp/model.ckpt'):
+
