3fffcaf238f68b44fc445948db7c8a20bd569d28
edward0rtiz <931@holbertonschool.com>
2020-05-23 20:58:20 -0500

    [fix] initialize beta and gamma with 1 and 0 respectively

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -20,9 +20,9 @@ def create_batch_norm_layer(prev, n, activation):
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     x = tf.layers.Dense(units=n, activation=None, kernel_initializer=init)
     x_prev = x(prev)
-    scale = tf.get_variable('gamma', shape=[n])
+    scale = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma')
     mean, variance = tf.nn.moments(x_prev, axes=[0])
-    offset = tf.get_variable('beta', shape=[n])
+    offset = tf.Variable(tf.constant(0.0, shape=[n]), name='beta')
     variance_epsilon = 1e-8
 
     normalization = tf.nn.batch_normalization(
