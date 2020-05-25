3660821f2213416c7708c62a09e371855a94b349
edward0rtiz <931@holbertonschool.com>
2020-05-25 12:11:58 -0500

    [feature] main files

---
+++ supervised_learning/0x04-error_analysis/main_files/0-main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import numpy as np
+create_confusion_matrix = __import__('0-create_confusion').create_confusion_matrix
+
+if __name__ == '__main__':
+    lib = np.load('../labels_logits.npz')
+    labels = lib['labels']
+    logits = lib['logits']
+
+    print("------------------")
+    print(labels)
+
+    print("------------------")
+    print(logits)
+
+    print("------------------")
+    print("------------------")
+
+    np.set_printoptions(suppress=True)
+    confusion = create_confusion_matrix(labels, logits)
+    print(confusion)
+    np.savez_compressed('../confusion.npz', confusion=confusion)
