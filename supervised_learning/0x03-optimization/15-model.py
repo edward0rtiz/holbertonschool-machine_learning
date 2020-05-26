5dedbf1dc75c8abe89e863e2bed78be9f16261a4
edward0rtiz <931@holbertonschool.com>
2020-05-25 23:58:45 -0500

    [fix] rm tf.assign

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -271,6 +271,5 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001,
                         print("\tStep {}:".format(step_number + 1))
                         print("\t\tCost: {}".format(step_cost))
                         print("\t\tAccuracy: {}".format(step_accuracy))
-            sess.run(tf.assing(global_step, global_step + 1))
             save_path = saver.save(sess, save_path)
     return save_path
