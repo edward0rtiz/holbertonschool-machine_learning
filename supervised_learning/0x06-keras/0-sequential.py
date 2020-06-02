2b3233c253567822cac98eb1bc5211456cf1b070
edward0rtiz <931@holbertonschool.com>
2020-06-02 10:53:50 -0500

    [fix] refactor keep_prob probability

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -28,7 +28,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                                      kernel_regularizer=L2,
                                      name='dense'))
         else:
-            model.add(K.layers.Dropout(keep_prob))
+            model.add(K.layers.Dropout(1 - keep_prob))
             model.add(K.layers.Dense(layers[i], activation=activations[i],
                                      kernel_regularizer=L2,
                                      name='dense_' + str(i)))
