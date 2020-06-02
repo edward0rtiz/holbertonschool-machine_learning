384cbc4ae6567eabd4e7aaf43a8168650205f2a9
edward0rtiz <931@holbertonschool.com>
2020-06-01 22:18:22 -0500

    [feature] implementing early stopping

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Script to train a model using keras"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """
+    Function to train a model using keras and validate data
+    Args:
+        network: model to train
+        data: numpy.ndarray of shape (m, nx) containing the input data
+        labels: one-hot numpy.ndarray of shape (m, classes) containing
+                the labels of data
+        batch_size: size of the batch used for mini-batch gradient descent
+        epochs: number of passes through data for mini-batch gradient descent
+        validation_data: data to validate the model with, if not None
+        early_stopping: boolean that indicates whether early stopping
+                        should be used
+        patience: patience used for early stopping
+        verbose: boolean that determines if output should be printed during
+                 training
+        shuffle: boolean that determines whether to shuffle the batches every
+                 epoch.
+    Returns: History object generated after training the model
+
+    """
+    if validation_data and early_stopping:
+        callback_early_stopping = [K.callbacks.EarlyStopping(patience=patience)]
+
+    history = network.fit(data, labels, epochs=epochs,
+                          batch_size=batch_size, verbose=verbose,
+                          validation_data=validation_data, shuffle=shuffle,
+                          callbacks=callback_early_stopping)
+    return history
