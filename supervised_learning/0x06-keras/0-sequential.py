503ecf8cfe05b5abf79ef99dc2cae97fd11d433e
edward0rtiz <931@holbertonschool.com>
2020-06-02 10:40:11 -0500

    [fix] reduce lined legnht ln12, ln14, ln28, ln33, create L2 var of K.regularizer

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -9,8 +9,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     Function to create a DNN using keras
     Args:
         nx: number of input features to the network
-        layers: list containing the number of nodes in each layer of the network
-        activations: list containing the activation functions used for each layer of the network
+        layers: list containing the number of nodes in each layer
+                of the network
+        activations: list containing the activation functions used
+                     for each layer of the network
         lambtha: L2 regularization parameter
         keep_prob: probability that a node will be kept for dropout
     Returns: Keras model
@@ -18,15 +20,16 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
 
     model = K.Sequential()
+    L2 = K.regularizers.l2(lambtha)
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i], input_shape=(nx,),
                                      activation=activations[i],
-                                     kernel_regularizer=K.regularizers.l2(lambtha),
+                                     kernel_regularizer=L2,
                                      name='dense'))
         else:
             model.add(K.layers.Dropout(keep_prob))
             model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=K.regularizers.l2(lambtha),
+                                     kernel_regularizer=L2,
                                      name='dense_' + str(i)))
     return model
