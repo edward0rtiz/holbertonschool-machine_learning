d4748cb24da749584164da2a5ccaf8bb9c86f511
edward0rtiz <931@holbertonschool.com>
2020-06-01 21:31:24 -0500

    [feature] optimize ADAM using keras

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Script to implement ADAM optimization using keras"""
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Funtion of ADAM optimization using keras
+    Args:
+        network: the model to optimize
+        alpha: learning rate
+        beta1: Adam optimization parameter
+        beta2: second Adam optimization parameter
+
+    Returns: none
+
+    """
+    ADAM = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=ADAM, loss='categorical_crossentropy', metrics='accuracy')
+    return None
