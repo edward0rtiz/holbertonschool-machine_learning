0b7b7c95df85dfcccdcc9e3a635511878a60f2d0
edward0rtiz <931@holbertonschool.com>
2020-06-02 12:10:26 -0500

    [fix] test appyling a var

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -23,7 +23,8 @@ def train_model(network, data, labels, batch_size, epochs,
     Returns: History object generated after training the model
 
     """
-    return network.fit(x=data, y=labels, epochs=epochs,
-                       batch_size=batch_size, shuffle=shuffle,
-                       verbose=verbose,
-                       validation_data=validation_data)
+    history = network.fit(data, labels, epochs=epochs,
+                          batch_size=batch_size, shuffle=shuffle,
+                          verbose=verbose,
+                          validation_data=validation_data)
+    return history
