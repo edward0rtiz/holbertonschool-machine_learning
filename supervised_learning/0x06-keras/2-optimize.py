661f2f0ce53ec1b0e3d440fba1400860d0be66e8
edward0rtiz <931@holbertonschool.com>
2020-06-01 21:57:31 -0500

    [fix] rename learning name for lr due documentation update

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -16,6 +16,6 @@ def optimize_model(network, alpha, beta1, beta2):
     Returns: none
 
     """
-    ADAM = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
-    network.compile(optimizer=ADAM, loss='categorical_crossentropy', metrics='accuracy')
+    ADAM = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=ADAM, loss='categorical_crossentropy', metrics=['accuracy'])
     return None
