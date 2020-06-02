a43b4ff4b623b4b1bd68f92f8c36a64f8be3b52c
edward0rtiz <931@holbertonschool.com>
2020-06-01 22:08:06 -0500

    [feature] validation data on train with keras

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Script to train a model using keras"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Function to train a model using keras and validate data
+    Args:
+        network: model to train
+        data: numpy.ndarray of shape (m, nx) containing the input data
+        labels: one-hot numpy.ndarray of shape (m, classes) containing
+                the labels of data
+        batch_size: size of the batch used for mini-batch gradient descent
+        epochs: number of passes through data for mini-batch gradient descent
+        validation_data: data to validate the model with, if not None
+        verbose: boolean that determines if output should be printed during
+                 training
+        shuffle: boolean that determines whether to shuffle the batches every
+                 epoch.
+    Returns: History object generated after training the model
+
+    """
+    history = network.fit(data, labels, epochs=epochs,
+                          batch_size=batch_size, verbose=verbose,
+                          validation_data=validation_data, shuffle=shuffle)
+    return history
