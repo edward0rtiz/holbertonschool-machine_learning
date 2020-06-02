dec95f7a6fe62d41a9cbccbc7e5970209cc9aae5
edward0rtiz <931@holbertonschool.com>
2020-06-02 11:10:34 -0500

    [fix] shorten ln12, ln13, create L2 var, fix dropout

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -9,8 +9,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     Function to create a DNN using keras
     Args:
         nx: number of input features to the network
-        layers: list containing the number of nodes in each layer of the network
-        activations: list containing the activation functions used for each layer of the network
+        layers: list containing the number of nodes in each
+                layer of the network
+        activations: list containing the activation functions
+                     used for each layer of the network
         lambtha: L2 regularization parameter
         keep_prob: probability that a node will be kept for dropout
     Returns: Keras model
@@ -18,13 +20,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
 
     inputs = K.Input(shape=(nx,))
+    L2 = K.regularizers.l2(lambtha)
     for i in range(len(layers)):
         if i == 0:
             output = K.layers.Dense(layers[i],
                                     activation=activations[i],
-                                    kernel_regularizer=K.regularizers.l2(lambtha))(inputs)
+                                    kernel_regularizer=L2)(inputs)
         else:
-            dropout = K.layers.Dropout(keep_prob)(output)
+            dropout = K.layers.Dropout(1 - keep_prob)(output)
             output = K.layers.Dense(layers[i], activation=activations[i],
-                                    kernel_regularizer=K.regularizers.l2(lambtha))(dropout)
+                                    kernel_regularizer=L2)(dropout)
     return K.models.Model(inputs=inputs, outputs=output)
