421d799157ae142a967d62d1d5ad2b79e19beaee
edward0rtiz <931@holbertonschool.com>
2020-06-04 15:57:43 -0500

    [fix] docstinrg notation and shorten lines for pycodestyle

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Script to perform a padding convolution"""
 
 import numpy as np
 
@@ -7,11 +8,13 @@ def convolve_grayscale_padding(images, kernel, padding):
     """
     Function to perform a graysclae convolution
     Args:
-        images: numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+        images: numpy.ndarray with shape (m, h, w) containing
+                multiple grayscale images
                 m: the number of images
                 h: height in pixels of the images
                 w: width in pixels of the images
-        kernel: numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+        kernel: numpy.ndarray with shape (kh, kw) containing
+                the kernel for the convolution
                 kn: the height of the kernel
                 kw: the width of the kernel
         padding: tuple of (ph, pw)
@@ -30,8 +33,8 @@ def convolve_grayscale_padding(images, kernel, padding):
     output_h = h + (2 * pad_h) - kh + 1
     output_w = w + (2 * pad_w) - kw + 1
 
-    image_pad = np.pad(images, pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                       mode='constant')
+    image_pad = np.pad(images, pad_width=((0, 0), (pad_h, pad_h),
+                                          (pad_w, pad_w)), mode='constant')
 
     # convolution output
     conv_out = np.zeros((m, output_h, output_w))
@@ -41,5 +44,7 @@ def convolve_grayscale_padding(images, kernel, padding):
     for x in range(output_h):
         for y in range(output_w):
             # element wise multiplication of the kernel and the image
-            conv_out[image, x, y] = (np.sum(image_pad[image, x:kh+x, y:kw+y] * kernel, axis=(1, 2)))
+            conv_out[image, x, y] = (np.sum(image_pad[image,
+                                            x:kh+x, y:kw+y] * kernel,
+                                            axis=(1, 2)))
     return conv_out
