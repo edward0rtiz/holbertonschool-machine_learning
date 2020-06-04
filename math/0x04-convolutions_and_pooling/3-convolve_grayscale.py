[fix] ln 42, ln 43 formula of padding

math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -39,8 +39,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sw = stride[1]
 
     if padding == 'same':
-        pad_h = int(((h - 1) * sh + kh - h) / 2) + 1
-        pad_w = int(((w - 1) * sw + kw - w) / 2) + 1
+        pad_h = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pad_w = int((((w - 1) * sw + kw - w) / 2) + 1)
     if type(padding) == tuple:
         pad_h = padding[0]
         pad_w = padding[1]
@@ -48,8 +48,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     image_pad = np.pad(images, pad_width=((0, 0), (pad_h, pad_h),
                                           (pad_w, pad_w)), mode='constant')
 
-    output_h = h + (2 * pad_h) - kh + 1
-    output_w = w + (2 * pad_w) - kw + 1
+    output_h = int(((h + 2 * pad_h - kh) / sh) + 1)
+    output_w = int(((w + 2 * pad_w - kh) / sw) + 1)
 
     # convolution output
     conv_out = np.zeros((m, output_h, output_w))
@@ -60,7 +60,7 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         for y in range(output_w):
             # element wise multiplication of the kernel and the image
             conv_out[image, x, y] = (np.sum(image_pad[image,
-                                            x * sh:(kh + (x * sh)),
-                                            y * sw:(kw + (y * sw))] * kernel,
+                                            x * sh:((x * sh) + kh),
+                                            y * sw:((y * sw) + kw)] * kernel,
                                             axis=(1, 2)))
     return conv_out