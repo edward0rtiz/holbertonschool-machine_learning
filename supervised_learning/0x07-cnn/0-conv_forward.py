66b737ef5727ca37f8bb341f15c8a3e5e8e84c5d
edward0rtiz <931@holbertonschool.com>
2020-06-08 16:41:50 -0500

    [fix] add more information to third loop

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -67,8 +67,9 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
         for j in range(c_w):
             for k in range(c_new):
                 # Use corners to define 3D slice of img_pad element wise
+                # sth = i * sh // endh =  ((i * sh) + kh)
                 img_slice = img_pad[:, i * sh:((i * sh) + kh),
-                                    j * sw:((j * sw) + kw)]
+                            j * sw:((j * sw) + kw)]
                 kernel = W[:, :, :, k]
                 biases = b[:, :, :, k]
                 conv[:, i, j, k] = (np.sum(img_slice * kernel,
