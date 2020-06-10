139138424cdfa1a5c3a66672e3da079c1774a7ea
edward0rtiz <931@holbertonschool.com>
2020-06-10 14:19:17 -0500

    [fix] remove + 1 add the ceil function

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -45,8 +45,8 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
     ph = 0
 
     if padding == 'same':
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+        ph = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
+        pw = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
     if padding == 'valid':
         ph = 0
         pw = 0
