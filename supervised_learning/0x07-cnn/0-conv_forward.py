[fix] replace ceil formula

supervised_learning/0x07-cnn/0-conv_forward.py
@@ -45,8 +45,8 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
     ph = 0
 
     if padding == 'same':
-        ph = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
-        pw = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
+        ph = int((((h_prev - 1) * sh + kh - h_prev) / 2) + 1)
+        pw = int((((w_prev - 1) * sw + kw - w_prev) / 2) + 1)
     if padding == 'valid':
         ph = 0
         pw = 0
@@ -77,6 +77,6 @@ def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
                 kernel = W[:, :, :, k]
                 conv[:, i, j, k] = (np.sum(np.multiply(img_slice,
                                                        kernel),
-                                                       axis=(1, 2, 3)))
+                                           axis=(1, 2, 3)))
     Z = conv + b
     return activation(Z)