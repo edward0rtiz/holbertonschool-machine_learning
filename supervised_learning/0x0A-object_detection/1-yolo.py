f17b8a9a781cd0de39bb45eaede172c09be8bd29
edward0rtiz <931@holbertonschool.com>
2020-07-01 16:00:04 -0500

    [fix] refactor image_h image_w at ln 64 to 67

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -50,17 +50,21 @@ class Yolo():
         for ipred, pred in enumerate(boxes):
             for grid_h in range(pred.shape[0]):
                 for grid_w in range(pred.shape[1]):
-                    cx = ((self.sigmoid(pred[grid_w, grid_h, :, 0]) + grid_w) / pred.shape[1] * image_size[1])
-                    cy = ((self.sigmoid(pred[grid_w, grid_h, :, 1]) + grid_h) / pred.shape[0] * image_size[0])
+                    bx = ((self.sigmoid(pred[grid_h, grid_w, :, 0]) + grid_w) / pred.shape[1])
+                    by = ((self.sigmoid(pred[grid_h, grid_w, :, 1]) + grid_h) / pred.shape[0])
+
                     anchor_tensor = self.anchors[ipred].astype(float)
-                    th = image_size[1] / self.model.input.shape[1].value
-                    tw = image_size[0] / self.model.input.shape[2].value
-                    anchor_tensor[:, 1] *= self.sigmoid(pred[grid_w, grid_h, :, 3]) / 2 * tw
-                    anchor_tensor[:, 0] *= self.sigmoid(pred[grid_w, grid_h, :, 2]) / 2 * th
-                    pred[grid_w, grid_h, :, 0] = cx - anchor_tensor[:, 0]  # x1
-                    pred[grid_w, grid_h, :, 1] = cy - anchor_tensor[:, 1]  # y1
-                    pred[grid_w, grid_h, :, 2] = cx + anchor_tensor[:, 0]  # x2
-                    pred[grid_w, grid_h, :, 3] = cy + anchor_tensor[:, 1]  # y2
+
+
+                    anchor_tensor[:, 0] *= np.exp(pred[grid_h, grid_w, :, 2]) / self.model.input.shape[1].value  # bw
+                    anchor_tensor[:, 1] *= np.exp(pred[grid_h, grid_w, :, 3]) / self.model.input.shape[2].value  # bh
+
+
+
+                    pred[grid_h, grid_w, :, 0] = (bx - (anchor_tensor[:, 0] / 2)) * image_size[1] # x1
+                    pred[grid_h, grid_w, :, 1] = (by - (anchor_tensor[:, 1] / 2)) * image_size[0] # y1
+                    pred[grid_h, grid_w, :, 2] = (bx + (anchor_tensor[:, 0] / 2)) * image_size[1]  # x2
+                    pred[grid_h, grid_w, :, 3] = (by + (anchor_tensor[:, 1] / 2)) * image_size[0]  # y2
         # box confidence
 
         box_confidences = [self.sigmoid(pred[:, :, :, 4:5]) for pred in outputs]
