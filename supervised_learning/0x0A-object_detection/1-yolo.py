f849aed466ae6b4d0093e3729a1023a41d1f8ea7
edward0rtiz <931@holbertonschool.com>
2020-07-02 09:39:31 -0500

    [fix] docstrings

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -40,35 +40,47 @@ class Yolo():
         Args:
             outputs: list of numpy.ndarrays containing the predictions from
                      the Darknet model for a single image:
-                        (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
-            image_size:
-
+                     (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
+            image_size: numpy.ndarray containing the image’s original size
+                        [image_height, image_width]
         Returns: (boxes, box_confidences, box_class_probs)
         """
-
         boxes = [pred[:, :, :, 0:4] for pred in outputs]
         for ipred, pred in enumerate(boxes):
             for grid_h in range(pred.shape[0]):
                 for grid_w in range(pred.shape[1]):
-                    bx = ((self.sigmoid(pred[grid_h, grid_w, :, 0]) + grid_w) / pred.shape[1])
-                    by = ((self.sigmoid(pred[grid_h, grid_w, :, 1]) + grid_h) / pred.shape[0])
-
+                    bx = ((self.sigmoid(pred[grid_h,
+                                        grid_w, :,
+                                        0]) + grid_w) / pred.shape[1])
+                    by = ((self.sigmoid(pred[grid_h,
+                                        grid_w, :,
+                                        1]) + grid_h) / pred.shape[0])
                     anchor_tensor = self.anchors[ipred].astype(float)
+                    anchor_tensor[:, 0] *= \
+                        np.exp(pred[grid_h, grid_w, :,
+                               2]) / self.model.input.shape[1].value  # bw
+                    anchor_tensor[:, 1] *= \
+                        np.exp(pred[grid_h, grid_w, :,
+                               3]) / self.model.input.shape[2].value  # bh
 
-
-                    anchor_tensor[:, 0] *= np.exp(pred[grid_h, grid_w, :, 2]) / self.model.input.shape[1].value  # bw
-                    anchor_tensor[:, 1] *= np.exp(pred[grid_h, grid_w, :, 3]) / self.model.input.shape[2].value  # bh
-
-
-
-                    pred[grid_h, grid_w, :, 0] = (bx - (anchor_tensor[:, 0] / 2)) * image_size[1] # x1
-                    pred[grid_h, grid_w, :, 1] = (by - (anchor_tensor[:, 1] / 2)) * image_size[0] # y1
-                    pred[grid_h, grid_w, :, 2] = (bx + (anchor_tensor[:, 0] / 2)) * image_size[1]  # x2
-                    pred[grid_h, grid_w, :, 3] = (by + (anchor_tensor[:, 1] / 2)) * image_size[0]  # y2
+                    pred[grid_h, grid_w, :, 0] = \
+                        (bx - (anchor_tensor[:, 0] / 2)) * \
+                        image_size[1]  # x1
+                    pred[grid_h, grid_w, :, 1] = \
+                        (by - (anchor_tensor[:, 1] / 2)) * \
+                        image_size[0]  # y1
+                    pred[grid_h, grid_w, :, 2] = \
+                        (bx + (anchor_tensor[:, 0] / 2)) * \
+                        image_size[1]  # x2
+                    pred[grid_h, grid_w, :, 3] = \
+                        (by + (anchor_tensor[:, 1] / 2)) * \
+                        image_size[0]  # y2
         # box confidence
 
-        box_confidences = [self.sigmoid(pred[:, :, :, 4:5]) for pred in outputs]
+        box_confidences = [self.sigmoid(pred[:, :, :,
+                                        4:5]) for pred in outputs]
 
         # box class probs
-        box_class_probs = [self.sigmoid(pred[:, :, :, 5:]) for pred in outputs]
+        box_class_probs = [self.sigmoid(pred[:, :, :,
+                                        5:]) for pred in outputs]
         return boxes, box_confidences, box_class_probs
