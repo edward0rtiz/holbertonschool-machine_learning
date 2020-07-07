6b16d247109301cb0fbfcd3bb1f4314f3b76639b
edward0rtiz <931@holbertonschool.com>
2020-07-07 09:02:34 -0500

    [update] add align function and landmarks points

--- supervised_learning/0x0B-face_verification/align.py
+++ supervised_learning/0x0B-face_verification/align.py
@@ -53,3 +53,52 @@ class FaceAlign:
             return rectangle
         except Exception:
             return None
+
+
+    def find_landmarks(self, image, detection):
+        """
+        Find landmarks
+        Args:
+            image: numpy.ndarray of an image from which to find
+                   facial landmarks
+            detection: dlib.rectangle containing the boundary
+                       box of the face in the image
+        Returns: numpy.ndarray of shape (p, 2)containing the
+                 landmark points, or None on failure
+        """
+        try:
+            shape = self.shape_predictor(image, detection)
+            coord = np.zeros((shape.num_parts, 2), dtype="int")
+            for i in range(0, shape.num_parts):
+                coord[i] = (shape.part(i).x, shape.part(i).y)
+
+            return coord
+        except Exception:
+            return None
+
+    def align(self, image, landmark_indices, anchor_points, size=96):
+        """
+        Function to align for face verification
+        Args:
+            image: numpy.ndarray of rank 3 containing the image to be
+                   aligned
+            landmark_indices: numpy.ndarray of shape (3,) containing
+                            the indices of the three landmark points
+                            that should be used for the affine
+                            transformation
+            anchor_points: numpy.ndarray of shape (3, 2) containing
+                           the destination points for the affine
+                           transformation, scaled to the range [0, 1]
+            size: the desired size of the aligned image
+        Returns: numpy.ndarray of shape (size, size, 3) containing the
+                 aligned image, or None if no face is detected
+        """
+        img = self.detect(image)
+        landmark = self.find_landmarks(image, img)
+        srcTri = landmark[landmark_indices]
+        srcTri = srcTri.astype(np.float32)
+        anchor = anchor_points * size
+        warp_mat = cv2.getAffineTransform(srcTri, anchor)
+        warp_dst = cv2.warpAffine(image, warp_mat, (size, size))
+
+        return warp_dst
