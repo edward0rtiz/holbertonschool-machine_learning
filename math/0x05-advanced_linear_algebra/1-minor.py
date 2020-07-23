6b67150c05d3149d0bf56b790eb7dfed01d71b5f
edward0rtiz <931@holbertonschool.com>
2020-07-23 13:11:33 -0500

    [fix] add lists edge case

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -80,15 +80,15 @@ def minor(matrix):
     range_mat_l = range(len(matrix))
 
     if type(matrix) != list or len(matrix) == 0:
-        raise TypeError("matrix must be a list of list")
+        raise TypeError("matrix must be a list of lists")
+    if not all([type(mat) == list for mat in matrix]):
+        raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
     if matrix[0] and len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a non-empty square matrix")
     if mat_l == 1:
         return [[1]]
-    if mat_l == 2 and len(matrix[0]) == 2:
-        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
 
     minor_values = []
     for row in range_mat_l:
