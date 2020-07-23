50d855deeb6e6f53a707c297e3fb2c10800abf27
edward0rtiz <931@holbertonschool.com>
2020-07-23 17:05:23 -0500

    [fix] add edge case for list of lists

--- math/0x05-advanced_linear_algebra/3-adjugate.py
+++ math/0x05-advanced_linear_algebra/3-adjugate.py
@@ -81,6 +81,8 @@ def adjugate(matrix):
 
     if type(matrix) != list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
+    if not all([type(mat) == list for mat in matrix]):
+        raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
     if matrix[0] and len(matrix) != len(matrix[0]):
