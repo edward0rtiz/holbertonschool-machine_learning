38320b927b88602aadbfe627ca27c2ad32a29521
edward0rtiz <931@holbertonschool.com>
2020-07-23 16:45:35 -0500

    [fix] add case len of mat

--- math/0x05-advanced_linear_algebra/2-cofactor.py
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -86,6 +86,8 @@ def cofactor(matrix):
         raise ValueError("matrix must be a non-empty square matrix")
     if matrix[0] and len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a non-empty square matrix")
+    if not all(mat_l == len(col) for col in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
     if mat_l == 1 and len(matrix[0]) == 1:
         return [[1]]
     if mat_l == 2 and len(matrix[0]) == 2:
