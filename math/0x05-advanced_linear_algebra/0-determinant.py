470d8f9e68572cf935688eaa190ce94b11631ef2
edward0rtiz <931@holbertonschool.com>
2020-07-23 12:15:56 -0500

    [fix] add case where mat_len is 1 and mat_len at 0 is 0

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -47,6 +47,8 @@ def determinant(matrix):
         return 1
     if mat_l == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
+    if mat_l == 1 and len(matrix[0]) == 0:
+        return 1
     if mat_l == 2 and len(matrix[0]) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
 
