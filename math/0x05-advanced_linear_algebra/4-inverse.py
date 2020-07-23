e90ccef9a92992a49d9bd2f7e81f660827f31809
edward0rtiz <931@holbertonschool.com>
2020-07-23 17:17:31 -0500

    [fix] ln 95 matrix order

--- math/0x05-advanced_linear_algebra/4-inverse.py
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -40,16 +40,17 @@ def determinant(matrix):
     """
     mat_l = len(matrix)
     if type(matrix) != list or len(matrix) == 0:
-        raise TypeError("Matrix must be a list of list")
+        raise TypeError("matrix must be a list of lists")
+    if not all([type(mat) == list for mat in matrix]):
+        raise TypeError("matrix must be a list of lists")
     if matrix[0] and mat_l != len(matrix[0]):
-        raise ValueError("Matrix must be a square matrix")
+        raise ValueError("matrix must be a square matrix")
+    if matrix == [[]]:
+        return 1
     if mat_l == 1 and len(matrix[0]) == 1:
-        return [[1]]
-    if mat_l == 1:
         return matrix[0][0]
-    if mat_l == 2 and len(matrix[0]) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
-
+    if not all(mat_l == len(col) for col in matrix):
+        raise ValueError("matrix must be a square matrix")
     return multi_determinant(matrix)
 
 
@@ -78,16 +79,20 @@ def adjugate(matrix):
     range_mat_l = range(len(matrix))
 
     if type(matrix) != list or len(matrix) == 0:
-        raise TypeError("matrix must be a list of list")
+        raise TypeError("matrix must be a list of lists")
+    if not all([type(mat) == list for mat in matrix]):
+        raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
     if matrix[0] and len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a non-empty square matrix")
+    if not all(mat_l == len(col) for col in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
     if mat_l == 1:
         return [[1]]
     if mat_l == 2 and len(matrix[0]) == 2:
         # apply a "checkerboard" of minuses to the "Matrix of Minors" +-+-
-        return [[matrix[1][1], -matrix[1][0]], [-matrix[0][1], matrix[0][0]]]
+        return [[matrix[1][1], -matrix[0][1]], [-matrix[1][0], matrix[0][0]]]
 
     minor_values = []
     for row in range_mat_l:
