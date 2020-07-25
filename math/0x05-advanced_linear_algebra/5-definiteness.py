0f680b60e876d3cba5eeff4624c29c51888f0d01
edward0rtiz <931@holbertonschool.com>
2020-07-24 21:25:58 -0500

    [feature] add definitess

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Definiteness functions"""
+
+import numpy as np
+from numpy import linalg as LA
+
+
+def definiteness(matrix):
+    """
+    Definiteness function
+    Args:
+        matrix: numpy.ndarray of shape (n, n)
+                whose definiteness should be calculated
+    Returns: positive definite, positive semi-definite,
+             negative semi-definite, negative definite of
+             indefinite, respectively
+    """
+    if type(matrix) is not np.ndarray:
+        raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) == 1:
+        return None
+    if (matrix.shape[0] != matrix.shape[1]):
+        return None
+
+    # w eigenvalues, v eigenvectors
+    w, v = LA.eig(matrix)
+    if np.all(w > 0):
+        return "Positive definite"
+    if np.all(w >= 0):
+        return "Positive semi-definite"
+    if np.all(w < 0):
+        return "Negative definite"
+    if np.all(w <= 0):
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"
