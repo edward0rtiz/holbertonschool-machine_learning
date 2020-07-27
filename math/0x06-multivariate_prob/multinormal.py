d12e882e442262afdeaded08388a2c5c63f3a6ad
edward0rtiz <931@holbertonschool.com>
2020-07-27 15:01:19 -0500

    [feature] multinormal pdf formula based on gaussian multivariate

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -25,3 +25,29 @@ class MultiNormal():
         self.mean = np.mean(data, axis=1).reshape(d, 1)
         X_mean = data - self.mean
         self.cov = np.matmul(X_mean, X_mean.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        PDF function
+        Args:
+            X: numpy.ndarray of shape (d, 1) containing the data point
+               whose PDF should be calculated
+
+        Returns:
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError('x must be a numpy.ndarray')
+        if len(x.shape) != 2:
+            raise ValueError('x must have the shape ({}, 1)',
+                             format(self.cov.shape[0]))
+        if x.shape[1] != 1:
+            raise ValueError('x must have the shape ({}, 1)',
+                             format(self.cov.shape[0]))
+        X_mean = x - self.mean
+
+        d = self.cov.shape[0]
+        p_a = 1 / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov)))
+        p_b = (np.exp(-(np.linalg.solve(self.cov, X_mean).T.dot(X_mean)) / 2))
+        pdf = float(p_a * p_b)
+
+        return pdf
