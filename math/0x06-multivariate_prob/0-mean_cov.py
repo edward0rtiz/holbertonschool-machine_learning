1fd60688c490a8c5b3ef752c96d529e474edfe2e
edward0rtiz <931@holbertonschool.com>
2020-07-27 13:07:01 -0500

    [fix] n and d refactor shape

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -12,13 +12,13 @@ def mean_cov(X):
         X:  numpy.ndarray of shape (n, d) of the data set
     Returns: mean, cov
     """
-    n = X.shape[0]
-    d = X.shape[1]
+
     if type(X) is not np.ndarray or len(X.shape) != 2:
         raise TypeError('X must be a 2D numpy.ndarray')
-    if n < 2:
+    if X.shape[0] < 2:
         raise ValueError('X must contain multiple data points')
 
+    n, d = X.shape
     mean = np.mean(X, axis=0).reshape(1, d)
     X_mean = X - mean
 
