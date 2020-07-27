379ad4f5eb5bb09c6db8777009c60238d1a5de78
edward0rtiz <931@holbertonschool.com>
2020-07-27 11:45:57 -0500

    [feature] add mean and covariance formulas

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Mean and covariance function"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Function that calculates the mean and covariance
+    Args:
+        X:  numpy.ndarray of shape (n, d) of the data set
+    Returns: mean, cov
+    """
+    n = X.shape[0]
+    d = X.shape[1]
+    if (type(X) != np.ndarray):
+        raise TypeError('X must be a 2D numpy.ndarray')
+    if n < 2:
+        raise ValueError('X must contain multiple data points')
+
+    mean = np.mean(X, axis=0).reshape(1, d)
+    X_mean = X - mean
+
+    cov = np.dot(X.T, X_mean) / (n - 1)
+
+    return mean, cov
