90e4b9cf76eb48ff73f0e9483951a2b98c602608
edward0rtiz <931@holbertonschool.com>
2020-07-29 08:59:54 -0500

    [feature] shannon entropy

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Shannon entropy and P affinities"""
+
+import numpy as np
+
+
+def HP(Di, beta):
+    """
+    Function that calculates shannon entropy
+    Args:
+        Di: numpy.ndarray of shape (n - 1,) containing the pairwise distances
+            between a data point and all other points except itself
+            n: number of data points
+        beta: value for the Gaussian distribution
+
+    Returns: (Hi, Pi)
+             Hi: the Shannon entropy of the points
+             Pi: a numpy.ndarray of shape (n - 1,) containing the P affinities
+             of the points
+    """
+    P = np.exp(-Di.copy() * beta)
+    sumP = np.sum(P)
+    Pi = P / sumP
+    Hi = -np.sum(Pi * np.log2(Pi))
+    return (Hi, Pi)
