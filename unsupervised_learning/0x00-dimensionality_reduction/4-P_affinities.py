4787eebf7bc8d0d6eae68a2780e66a14e094fb80
edward0rtiz <931@holbertonschool.com>
2020-07-29 09:42:55 -0500

    [fix] remove set at n, d

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -16,7 +16,7 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
         perplexity:
     Returns:
     """
-    (n, d) = X.shape
+    n, d = X.shape
     D, P, betas, H = P_init(X, perplexity)
 
     for i in range(n):
@@ -30,15 +30,15 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
             if Hdiff > 0:
                 b_min = betas[i, 0]
                 if b_max is None:
-                    betas[i, 0] = betas[i, 0] * 2.
+                    betas[i, 0] = betas[i, 0] * 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + b_max) / 2.
+                    betas[i, 0] = (betas[i, 0] + b_max) / 2
             else:
                 b_max = betas[i, 0]
                 if b_min is None:
-                    betas[i, 0] = betas[i, 0] / 2.
+                    betas[i, 0] = betas[i, 0] / 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + b_min) / 2.
+                    betas[i, 0] = (betas[i, 0] + b_min) / 2
 
             Hi, Pi = HP(row, betas[i, 0])
             Hdiff = Hi - H
