19d07cf6232916e34963ebba9da4c8ae757e0203
edward0rtiz <931@holbertonschool.com>
2020-07-29 17:50:08 -0500

    [fix] add 0 to diagonal

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -21,15 +21,16 @@ def P_init(X, perplexity):
             H: is the Shannon entropy for perplexity perplexity
     """
 
-    n = X.shape[0]
+    n, d = X.shape
     # e distance for all pairs of points in input matrix X
     # ||Xi - Xj|| ** 2
     sum_X = np.sum(np.square(X), axis=1)
     # exp(-||Yi - Yj|| ** 2)
     D = (np.add(np.add(-2 * np.matmul(X, X.T), sum_X).T, sum_X))
+    # fill diagonal of 0
+    np.fill_diagonal(D, 0)
     P = np.zeros((n, n))
     betas = np.ones((n, 1))
-    #
     H = np.log2(perplexity)
     return D, P, betas, H
 
