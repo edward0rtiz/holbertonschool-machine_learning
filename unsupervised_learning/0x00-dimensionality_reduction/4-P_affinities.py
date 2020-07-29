11edc5a9fc798b570dbce0b64d37a623ce975a6f
edward0rtiz <931@holbertonschool.com>
2020-07-29 09:36:58 -0500

    [remove param d to _

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -16,7 +16,7 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
         perplexity:
     Returns:
     """
-    (n, d) = X.shape
+    (n, _) = X.shape
     D, P, betas, H = P_init(X, perplexity)
 
     for i in range(n):
