c90a58f09a4b8f113e575d8f0e136bdf2cb7eb35
edward0rtiz <931@holbertonschool.com>
2020-07-30 11:22:43 -0500

    [fix] typo at np.random.randn

--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -32,8 +32,7 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
 
     X = pca(X, idims)
     P = P_affinities(X, perplexity=perplexity)
-    Y = np.random.rand(n, ndims)
-    # iY = np.zeros((n, ndims))
+    Y = np.random.randn(n, ndims)
     iY = Y
     # early exaggeration
     P = P * 4.
@@ -41,17 +40,16 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     for i in range(iterations):
 
         dY, Q = grads(Y, P)
-        if i < 20:
+        if i <= 20:
             momentum = initial_momentum
         else:
             momentum = final_momentum
 
         # delta-bar-delta algorithm for SDG optional
-        """gains = (gains + 0.2) * ((dY > 0.) != (iY > 0.)) + \
-                (gains * 0.8) * ((dY > 0.) == (iY > 0.))
-        gains[gains < min_gain] = min_gain"""
+        # gains = (gains + 0.2) * ((dY > 0.) != (iY > 0.)) + \
+        #        (gains * 0.8) * ((dY > 0.) == (iY > 0.))
+        # gains[gains < min_gain] = min_gain"""
 
-        # iY = momentum * iY - lr * (gains * dY)
         iY = momentum * iY - lr * dY
         Y = Y + iY
         Y = Y - np.tile(np.mean(Y, 0), (n, 1))
@@ -59,6 +57,7 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
         if (i + 1) % 100 == 0:
             C = cost(P, Q)
             print('Cost at iteration {}: {}'.format((i+1), C))
+
         if i == 100:
             P = P / 4.
 
