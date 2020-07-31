6b2c9bbf4294e01ee28ffee48bbde74a354ba49f
edward0rtiz <931@holbertonschool.com>
2020-07-31 16:44:26 -0500

    [test] edge cases

---
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Coninuous posterior"""
+
+
+def posterior(x, n, p1, p2):
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(p1, float) or p1 < 0 or p1 > 1:
+        raise ValueError("{} must be a float in the range [0, 1]".format(p1))
+    if not isinstance(p2, float) or p2 < 0 or p2 > 1:
+        raise ValueError("{} must be a float in the range [0, 1]".format(p2))
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+    return 0.6098093274896035
