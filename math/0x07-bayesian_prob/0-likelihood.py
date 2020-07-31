0af6c228976847ba149a004f31956b0def6a98df
edward0rtiz <931@holbertonschool.com>
2020-07-31 14:50:44 -0500

    [fix] len 25 add edge case the len of shape

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -22,7 +22,7 @@ def likelihood(x, n, P):
             'x must be an integer that is greater than or equal to 0')
     if x > n:
         raise ValueError('x cannot be greater than n')
-    if not isinstance(P, np.ndarray):
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError('All values in P must be in the range [0, 1]')
