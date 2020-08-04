00f29fd082d1def06459a1aba79c1c628f6c97cd
edward0rtiz <931@holbertonschool.com>
2020-08-03 21:06:36 -0500

    [fix] add edge cases

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -16,9 +16,16 @@ def initialize(X, k):
     Returns: numpy.ndarray of shape (k, d) containing the initialized
              centroids for each cluster, or None on failure
     """
+    # cases
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    # Setting min and max values per col
     n, d = X.shape
     X_min = X.min(axis=0)
     X_max = X.max(axis=0)
 
+    # return multivariate uniform distribution
     return np.random.uniform(X_min, X_max, size=(k, d))
-
