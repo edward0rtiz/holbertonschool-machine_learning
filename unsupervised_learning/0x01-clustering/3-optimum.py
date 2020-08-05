52d54827112e8fda65488184c795f715fb3365da
edward0rtiz <931@holbertonschool.com>
2020-08-04 19:07:28 -0500

    [fix] remove edge cse and fix pycodestyle

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -26,15 +26,12 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     try:
         if not isinstance(X, np.ndarray) or len(X.shape) != 2:
             return None, None
-        if not isinstance(iterations, int) or  iterations <= 0:
+        if not isinstance(iterations, int) or iterations <= 0:
             return None, None
         if kmax is not None and (type(kmax) is not int or kmax < 1):
             return None, None
         if kmax is not None and kmin >= kmax:
             return None, None
-
-        if X.shape[1] != C.shape[1]:
-            return None
 
         results = []
         d_vars = []
