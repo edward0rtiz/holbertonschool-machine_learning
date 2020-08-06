5dcb2b7f6f8ff1af7b887a65fd69d6d58cac6546
edward0rtiz <931@holbertonschool.com>
2020-08-06 13:12:21 -0500

    [fix] add keepdims true ln 34

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -31,7 +31,7 @@ def maximization(X, g):
     k = gaussian_components.shape[0]
     n, d = X.shape
 
-    posterior_prob = np.sum(gaussian_components, axis=0)
+    posterior_prob = np.sum(gaussian_components, axis=0, keepdims=True)
     check = np.sum(posterior_prob)
     if check != X.shape[0]:
         return None, None, None
