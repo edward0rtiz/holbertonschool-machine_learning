47b6630b527cca5d1181c4fdf9d1746b8f648529
edward0rtiz <931@holbertonschool.com>
2020-08-06 13:17:41 -0500

    [fix] femore parenthesis

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -37,13 +37,13 @@ def expectation_maximization(X,
         l: log likelihood of the model
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return (None, None, None, None, None)
+        return None, None, None, None, None
     if type(k) != int or k <= 0 or k >= X.shape[0]:
-        return (None, None, None, None, None)
+        return None, None, None, None, None
     if type(iterations) != int or iterations <= 0:
-        return (None, None, None, None, None)
+        return None, None, None, None, None
     if type(tol) != float or tol <= 0:
-        return (None, None, None, None, None)
+        return None, None, None, None, None
     if type(verbose) != bool:
         return None, None, None, None, None
 
