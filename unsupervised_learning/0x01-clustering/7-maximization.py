fb8f8309a466896b3eff5c7e0e0e2e43e65864f5
edward0rtiz <931@holbertonschool.com>
2020-08-05 22:57:11 -0500

    [add edge case brute force shape comparison of g with X in ax[0]

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -31,8 +31,9 @@ def maximization(X, g):
     k = gaussian_components.shape[0]
     n, d = X.shape
 
-    posterior_prob = gaussian_components.shape[0]
-    if posterior_prob != X.shape[0]:
+    posterior_prob = np.sum(gaussian_components, axis=0)
+    check = np.sum(posterior_prob)
+    if check != X.shape[0]:
         return None, None, None
 
     # pi
