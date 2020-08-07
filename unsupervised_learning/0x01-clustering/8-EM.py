566b60d7060f307213fa9df119848babb4147855
edward0rtiz <931@holbertonschool.com>
2020-08-06 23:12:05 -0500

    [fix] remove ln 65

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -57,10 +57,10 @@ def expectation_maximization(X,
         if verbose is True and (i % 10 == 0):
             print("Log Likelihood after {} iterations: {}".format(
                 i, loglikelihood_new.round(5)))
-        if np.abs(loglikelihood - loglikelihood_new) < tol:
+        if np.abs(loglikelihood - loglikelihood_new) <= tol:
             break
         loglikelihood = loglikelihood_new
     if verbose is True:
         print("Log Likelihood after {} iterations: {}".format(
-            i + 1, loglikelihood_new.round(5)))
+            i, loglikelihood_new.round(5)))
     return pi, m, S, g, loglikelihood_new
