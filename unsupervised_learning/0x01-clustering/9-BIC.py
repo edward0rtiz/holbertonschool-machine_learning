1060fad49a2ac2281d3d1c3bb3eb25a5b1d3eeb3
edward0rtiz <931@holbertonschool.com>
2020-08-08 15:17:17 -0500

    [fix] order oof vars in ln 58

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -55,7 +55,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     bic_val = []
     n, d = X.shape
     for k in range(kmin, kmax + 1):
-        pi, m, S, log_l, _ = expectation_maximization(X, k, iterations, tol,
+        pi, m, S,  _, log_l = expectation_maximization(X, k, iterations, tol,
                                                       verbose)
         k_best.append(k)
         best_res.append((pi, m, S))
@@ -68,11 +68,12 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         p = int(cov_params + mean_params + k - 1)
 
         # Formula for this task BIC = p * ln(n) - 2 * l
-        BIC = p * np.log(n) - 2 * log_l
-        bic_val.append(BIC)
+        bic = p * np.log(n) - 2 * log_l
+        bic_val.append(bic)
+
     bic_val = np.array(bic_val)
-    best_val = np.argmin(bic_val)
     logl_val = np.array(logl_val)
+    best_val = np.argmin(bic_val)
 
     k_best = k_best[best_val]
     best_res = best_res[best_val]
