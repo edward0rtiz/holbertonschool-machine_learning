b800fbd9e5fe9841214a1827cdb63255e71029ec
edward0rtiz <931@holbertonschool.com>
2020-08-27 00:21:43 -0500

    [feature] vanilla encoder implementation

---
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -0,0 +1,53 @@
+#!usr/bin/env python3
+"""Basic autoencoder vanilla encoder"""
+
+import tensorflow.keras as K
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    autoencoder for nanilla
+    Args:
+        input_dims: integer containing the dimensions of the model input
+        hidden_layers: list containing the number of nodes for each hidden
+                       layer in the encoder, respectively
+                       hidden layers should be reversed for the decoder
+        latent_dims: integer containing the dimensions of the latent space
+                      representation
+    Returns: encoder, decoder, auto
+            encoder: the encoder model
+            decoder: the decoder model
+            auto: the full autoencoder model
+    """
+    X_input_encoded = K.layers.Input(shape=(input_dims,))
+    hidden_ly = K.layers.Dense(units=hidden_layers[0], activation='relu')
+
+    # reversed for the decoder
+    Y_prev = hidden_ly(X_input_encoded)
+    for i in range(1, len(hidden_ly)):
+        hidden_ly = K.layers.Dense(units=hidden_layers[i], activation='relu')
+        Y_prev = hidden_ly(Y_prev)
+
+    latent_ly = K.layers.Dense(units=latent_dims, activation='relu')
+    Y_encoded = latent_ly(Y_prev)
+    encoder = K.Model(inputs=X_input_encoded, output=Y_encoded)
+
+    X_input_decoded = K.Input(shape=(latent_dims,))
+    hidden_ly_decoded = K.layers.Dense(units=hidden_layers[-1],
+                                       activation='relu')
+    Y_prev = hidden_ly_decoded(X_input_decoded)
+    for j in range(len(hidden_layers) - 2, -1, -1):
+        hidden_ly_decoded = K.layers.Dense(units=hidden_layers[j],
+                                           activation='relu')
+        Y_prev = hidden_ly_decoded(Y_prev)
+    last_layer = K.layers.Dense(units=input_dims, activation='sigmoid')
+    output = last_layer(Y_prev)
+    decoder = K.Model(inputs=X_input_decoded, outputs=output)
+
+    X_input = K.Input(shape=(input_dims,))
+    encoder = encoder(X_input)
+    decoder = decoder(encoder)
+    auto = K.Model(inputs=X_input, outputs=decoder)
+    auto.compile(loss='binary_crossentropy', optimizer='adam')
+
+    return encoder, decoder, auto
