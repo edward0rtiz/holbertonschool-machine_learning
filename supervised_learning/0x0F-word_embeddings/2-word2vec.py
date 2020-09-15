0536e4e7382a5cea8514d18553cfd908d597df7e
edward0rtiz <931@holbertonschool.com>
2020-09-15 10:29:04 -0500

    [feature] add word2vec using gensim

---
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""word2vec technique"""
+
+import gensim
+
+
+def word2vec_model(sentences, size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   iterations=5, seed=0, workers=1):
+    """
+    word2vec using gensim
+    Args:
+        sentences: list of sentences to be trained on
+        size: dimensionality of the embedding layer
+        min_count: the minimum number of occurrences of a
+                    word for use in training
+        window: maximum distance between the current and
+                predicted word within a sentence
+        negative: the size of negative sampling
+        cbow: boolean to determine the training type; True
+              is for CBOW; False is for Skip-gram
+        iterations:  number of iterations to train over
+        seed: seed for the random number generator
+        workers: number of worker threads to train the model
+    Returns: the trained model
+    """
+    model = gensim.models.Word2Vec(sentences, min_count=min_count,
+                                   iter=iterations, size=size,
+                                   window=window, negative=negative,
+                                   seed=seed, sg=cbow, workers=workers)
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.iter)
+
+    return model
