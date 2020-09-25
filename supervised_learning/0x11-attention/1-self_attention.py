b8d209e2f7e472467e127d984704957a23675e28
edward0rtiz <931@holbertonschool.com>
2020-09-25 13:46:00 -0500

    [fix] doc string

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 """ Machine Translation model with RNN's """
 
 import tensorflow as tf
@@ -6,13 +6,21 @@ import tensorflow as tf
 
 class SelfAttention(tf.keras.layers.Layer):
     """
-
+    Class Self attention
     """
     def __init__(self, units):
         """
-
+        method initializer
         Args:
-            units:
+            units: Integer representing the number of hidden units in the
+                   alignment model
+
+            W - Dense layer with units units, to be applied to the previous
+                decoder hidden state
+            U - Dense layer with units units, to be applied to the
+                encoder hidden states
+            V - Dense layer with 1 units, to be applied to the tanh of
+                the sum of the outputs of W and U
         """
         super(SelfAttention, self).__init__()
         self.W = tf.keras.layers.Dense(units)
@@ -21,12 +29,17 @@ class SelfAttention(tf.keras.layers.Layer):
 
     def call(self, s_prev, hidden_states):
         """
-
+        call method
         Args:
-            s_prev:
-            hidden_states:
-
-        Returns:
+            s_prev: tensor of shape (batch, units) containing the previous
+                    decoder hidden state
+            hidden_states: tensor of shape (batch, input_seq_len, units)
+                           containing the outputs of the encoder
+        Returns: context, weights
+                 context: Tensor of shape (batch, units) that contains
+                          the context vector for the decoder
+                 weights: Tensor of shape (batch, input_seq_len, 1)
+                          that contains the attention weights
         """
         w = self.W(s_prev)
         u = self.U(hidden_states)
