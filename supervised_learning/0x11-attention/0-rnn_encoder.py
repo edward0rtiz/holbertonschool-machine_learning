7d322c4ea966808cb36dc119570c711f78a5a083
edward0rtiz <931@holbertonschool.com>
2020-09-25 13:15:53 -0500

    [fix] minnor issues

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -3,30 +3,36 @@
 
 import tensorflow as tf
 
+
 class RNNEncoder(tf.keras.layers.Layer):
     """
-
+    class rnn encoder
     """
     def __init__(self, vocab, embedding, units, batch):
         """
-
+        Inititalizer function
         Args:
-            vocab:
-            embedding:
-            units:
-            batch:
+            vocab: integer representing the size of the input vocabulary
+            embedding: integer representing the dimensionality of the embedding
+                       vector
+            units: integer representing the number of hidden units in the RNN
+                   cell
+            batch: integer representing the batch size
         """
         super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-        self.gru = tf.keras.layers.GRU(units, recurrent_initializer='glorot_uniform',
-                                       return_sequences=True, return_state=True)
+        self.gru = tf.keras.layers.GRU(units,
+                                       recurrent_initializer='glorot_uniform',
+                                       return_sequences=True,
+                                       return_state=True)
 
     def initialize_hidden_state(self):
         """
-
-        Returns:
+        Inititalize hidden states
+        Returns: tensor of shape (batch, units) containing the initialized
+                 hidden states
         """
         initializer = tf.keras.initializers.Zeros()
         # Q equals to matrix
@@ -35,12 +41,18 @@ class RNNEncoder(tf.keras.layers.Layer):
 
     def call(self, x, initial):
         """
-
+        Call methods
         Args:
-            x:
-            initial:
-
-        Returns:
+            x: Tensor of shape (batch, input_seq_len) containing the input
+                to the encoder
+               layer as word indices within the vocabulary
+            initial: tensor of shape (batch, units) containing the initial
+                     hidden state
+        Returns: outputs, hidden
+                Outputs: Tensor of shape (batch, input_seq_len, units)
+                         containing the outputs of the encoder
+                Hidden: Tensor of shape (batch, units) containing the last
+                        hidden state of the encoder
         """
         embedding = self.embedding(x)
         outputs, last_hiddenQ = self.gru(embedding,
