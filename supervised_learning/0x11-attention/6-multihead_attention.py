4e210b1e492944e2cf0dd65aad19ac483f1d2085
edward0rtiz <931@holbertonschool.com>
2020-09-25 23:07:20 -0500

    [fix]ln51

--- supervised_learning/0x11-attention/6-multihead_attention.py
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -48,7 +48,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         weights: tensor with its last three dimensions as
                 (..., h, seq_len_q, seq_len_v) containing the attention weights
         """
-        batch = V.shape[0]
+        batch = Q.shape[0]
 
         V_linear = self.Wv(V)
         K_linear = self.Wk(K)
