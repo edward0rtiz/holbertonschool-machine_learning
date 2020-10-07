aca7e05e3164a98ce6827cde40b7d72e2b4932be
edward0rtiz <931@holbertonschool.com>
2020-10-06 22:29:38 -0500

    [feature] dataset

---
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -0,0 +1,25 @@
+class Dataset():
+    """ class dataset """
+
+    def __init__(self):
+        """ initialize dataset """
+
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True,
+                                       as_supervised=True)
+
+        self.data_train, self.data_valid = examples['train'], \
+            examples['validation']
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """tokenize data """
+
+        tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+
+        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+
+        return tokenizer_pt, tokenizer_en
