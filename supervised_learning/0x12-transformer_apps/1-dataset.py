a4acd1f4f922db6f39eef4fa2ea3b609f748e214
edward0rtiz <931@holbertonschool.com>
2020-10-06 22:45:50 -0500

    [feature] add dataset

---
+++ supervised_learning/0x12-transformer_apps/1-dataset.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Class Dataset"""
+
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset():
+    """class dataset"""
+
+    def __init__(self):
+        """Class constructor"""
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True,
+                                       as_supervised=True)
+        self.data_train, self.data_valid = examples['train'], \
+            examples['validation']
+
+        self.tokenizer_pt, self.tokenizer_en = \
+            self.tokenize_dataset(self.data_train)
+
+    def tokenize_dataset(self, data):
+        """tokenize data """
+
+        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data),
+            target_vocab_size=2 ** 15)
+
+        tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data),
+            target_vocab_size=2 ** 15)
+
+        return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """ encoding """
+
+        lang1 = [self.tokenizer_pt.vocab_size] + self.tokenizer_pt.encode(
+            pt.numpy()) + [self.tokenizer_pt.vocab_size + 1]
+
+        lang2 = [self.tokenizer_en.vocab_size] + self.tokenizer_en.encode(
+            en.numpy()) + [self.tokenizer_en.vocab_size + 1]
+
+        return lang1, lang2
