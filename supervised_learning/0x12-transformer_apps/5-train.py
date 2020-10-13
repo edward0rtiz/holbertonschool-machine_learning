9a2fbdf46d5fdfb2ea7e5bd166a485b06a66539c
edward0rtiz <931@holbertonschool.com>
2020-10-13 16:40:02 -0500

    [fix] trani

--- supervised_learning/0x12-transformer_apps/5-train.py
+++ supervised_learning/0x12-transformer_apps/5-train.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Training Transformer"""
-
+"""Training Transformer"""
 import tensorflow.compat.v2 as tf
 Dataset = __import__('3-dataset').Dataset
 create_masks = __import__('4-create_masks').create_masks
@@ -8,9 +7,9 @@ Transformer = __import__('5-transformer').Transformer
 
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    """ class CustomSchedule"""
+    """class CustomSchedule"""
     def __init__(self, d_model, warmup_steps=4000):
-        """init method"""
+        """constructor"""
         super(CustomSchedule, self).__init__()
 
         self.d_model = d_model
@@ -24,3 +23,70 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         arg2 = step * (self.warmup_steps ** -1.5)
 
         return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
+    """train transformers"""
+    data = Dataset(batch_size, max_len)
+    learning_rate = CustomSchedule(dm)
+    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9,
+                                         beta_2=0.98, epsilon=1e-9)
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+    def loss_function(real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = loss_object(real, pred)
+
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
+
+        return tf.reduce_sum(loss_) / tf.reduce_sum(mask)
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name='train_accuracy')
+
+    input_vocab_size = data.tokenizer_pt.vocab_size + 2
+    target_vocab_size = data.tokenizer_en.vocab_size + 2
+
+    transformer = Transformer(N, dm, h, hidden,
+                              input_vocab_size, target_vocab_size,
+                              max_len, max_len)
+    def train_step(inp, tar):
+        tar_inp = tar[:, :-1]
+        tar_real = tar[:, 1:]
+
+        enc_padd_mask, com_mask, dec_padd_mask = create_masks(inp, tar_inp)
+
+        with tf.GradientTape() as tape:
+            predictions, _ = transformer(inp, tar_inp,
+                                         True,
+                                         enc_padd_mask,
+                                         com_mask,
+                                         dec_padd_mask)
+            loss = loss_function(tar_real, predictions)
+
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients,
+                                      transformer.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(tar_real, predictions)
+
+    for epoch in range(epochs):
+
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        # inp -> portuguese, tar -> english
+        for (batch, (inp, tar)) in enumerate(data.data_train):
+            train_step(inp, tar)
+
+            if batch % 50 == 0:
+                print('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                    epoch + 1, batch, train_loss.result(),
+                    train_accuracy.result()))
+
+        a = 'Epoch {}'.format(epoch + 1)
+        b = 'Loss {:.4f}'.format(train_loss.result())
+        print('{} {} Accuracy {:.4f}'.format(a, b, train_accuracy.result()))
