26d522cce9fc04bda194a29aa102cfa265467b2f
edward0rtiz <931@holbertonschool.com>
2020-10-26 18:58:12 -0500

    [feature] 12 hirecrchy

---
+++ pipeline/0x00-pandas/12-hierarchy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+
+df1 = df1.set_index('Timestamp')
+df2 = df2.set_index('Timestamp')
+df = pd.concat([df2.loc["1417411980":"1417417980"],
+                df1.loc["1417411980":"1417417980"]],
+               keys=["bitstamp", "coinbase"])
+df = df.swaplevel(0, 1, axis=0)
+df = df.sort_index()
+print(df)
