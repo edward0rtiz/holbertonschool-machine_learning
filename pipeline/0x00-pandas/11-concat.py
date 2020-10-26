681ba33fa5f3c8177e13cd06b6c223bf007293a6
edward0rtiz <931@holbertonschool.com>
2020-10-26 18:32:56 -0500

    [fix] concat value

--- pipeline/0x00-pandas/11-concat.py
+++ pipeline/0x00-pandas/11-concat.py
@@ -8,8 +8,8 @@ df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
 df1 = df1.set_index('Timestamp')
 df2 = df2.set_index('Timestamp')
-df1 = df1.loc["1417411980":"1417417980"]
-df2 = df2.loc["1417411980":"1417417980"]
+df1 = df1.loc["1417411920":"1417417920"]
+df2 = df2.loc["1417411920":"1417417920"]
 
 df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
 print(df)
