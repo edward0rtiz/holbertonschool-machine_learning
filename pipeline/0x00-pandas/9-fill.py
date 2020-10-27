a1090810416c48102c34f91fd3bf83f049ffabcb
edward0rtiz <931@holbertonschool.com>
2020-10-27 10:21:15 -0500

    [fix] use sift to get previos from close

--- pipeline/0x00-pandas/9-fill.py
+++ pipeline/0x00-pandas/9-fill.py
@@ -6,10 +6,10 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 df = df.drop(columns=['Weighted_Price'])
-df["Open"].fillna(method='ffill', inplace=True)
-df["High"].fillna(method='ffill', inplace=True)
-df["Low"].fillna(method='ffill', inplace=True)
 df["Close"].fillna(method='ffill', inplace=True)
+df["Open"].fillna(value=df['Close'].shift(1, fill_value=0), inplace=True)
+df["High"].fillna(value=df['Close'].shift(1, fill_value=0), inplace=True)
+df["Low"].fillna(value=df['Close'].shift(1, fill_value=0), inplace=True)
 df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
 df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
 print(df.head())
