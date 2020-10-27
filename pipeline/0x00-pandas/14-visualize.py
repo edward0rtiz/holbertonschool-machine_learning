7d422d685600f419ba90e1092e0e8a4ac4c08670
edward0rtiz <931@holbertonschool.com>
2020-10-26 21:52:56 -0500

    [fix] date init

--- pipeline/0x00-pandas/14-visualize.py
+++ pipeline/0x00-pandas/14-visualize.py
@@ -6,12 +6,11 @@ import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
 df = from_file("coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv", ',')
 
-df = df.loc[df["Timestamp"] >= 1483228800]
 df = df.rename(columns={"Timestamp": "Date"})
 df["Date"] = pd.to_datetime(df["Date"], unit='s')
+df= df[df['Date'] >= '2017-01-01']
 df = df.set_index("Date")
 
 df = df.drop(["Weighted_Price"], axis=1)
