f2c9b3c1f2185b4760bbf2e3b083e9108aef3ce7
edward0rtiz <931@holbertonschool.com>
2020-10-28 10:55:18 -0500

    [fix] add documentation and fix pycodestyle

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 import requests
 
+
 def availableShips(passengerCount):
+    """
+    Function to get request
+    Args:
+        passengerCount: number of passangers
+    Returns: List of starships
+    """
     starships = []
     url = 'https://swapi-api.hbtn.io/api/starships/'
     while url is not None:
-        response = requests.get(url, headers={'Accept': 'application/json'}, params={"term":'starships'})
+        response = requests.get(url,
+                                headers={'Accept': 'application/json'},
+                                params={"term": 'starships'})
         for ship in response.json()['results']:
             passenger = ship['passengers']
             if passenger.isnumeric() and int(passenger) >= passengerCount:
