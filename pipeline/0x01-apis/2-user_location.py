2eb0283b1c2afebfd20cfb1f2ce64d3657225952
edward0rtiz <931@holbertonschool.com>
2020-10-28 15:52:22 -0500

    [fix] testing

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -6,6 +6,7 @@ import time
 
 
 if __name__ == '__main__':
+    """
     url = sys.argv[1]
     headers = {'Accept': 'application/vnd.github.v3+json'}
     response = requests.get(url, headers=headers)
@@ -18,3 +19,16 @@ if __name__ == '__main__':
         start = int(time.time())
         elapsed = int((limit - start) / 60)
         print('Reset in {} min'.format(int(elapsed)))
+    """
+    url = sys.argv[1]
+    payload = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, params=payload)
+    if r.status_code == 403:
+        limit = r.headers["X-Ratelimit-Reset"]
+        x = (int(limit) - int(time.time())) / 60
+        print("Reset in {} min".format(int(x)))
+    if r.status_code == 200:
+        location = r.json()["location"]
+        print(location)
+    if r.status_code == 404:
+        print("Not found")
