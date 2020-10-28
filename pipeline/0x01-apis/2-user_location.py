4b9b9f14e2296df7b6952192c31cc87601015459
edward0rtiz <931@holbertonschool.com>
2020-10-28 15:55:05 -0500

    [add another int to ln18

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -6,7 +6,7 @@ import time
 
 
 if __name__ == '__main__':
-    """
+
     url = sys.argv[1]
     headers = {'Accept': 'application/vnd.github.v3+json'}
     response = requests.get(url, headers=headers)
@@ -15,20 +15,7 @@ if __name__ == '__main__':
     if response.status_code == 404:
         print('Not found')
     if response.status_code == 403:
-        limit = response.headers['X-Ratelimit-Reset']
+        limit = int(response.headers['X-Ratelimit-Reset'])
         start = int(time.time())
         elapsed = int((limit - start) / 60)
         print('Reset in {} min'.format(int(elapsed)))
-    """
-    url = sys.argv[1]
-    payload = {'Accept': 'application/vnd.github.v3+json'}
-    r = requests.get(url, params=payload)
-    if r.status_code == 403:
-        limit = r.headers["X-Ratelimit-Reset"]
-        x = (int(limit) - int(time.time())) / 60
-        print("Reset in {} min".format(int(x)))
-    if r.status_code == 200:
-        location = r.json()["location"]
-        print(location)
-    if r.status_code == 404:
-        print("Not found")
