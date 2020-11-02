c7034ac2c59bdb75a5a335907e66ca3d1cd1d1a5
edward0rtiz <931@holbertonschool.com>
2020-11-02 17:02:22 -0500

    [fix] ln3 change name of show id to genre_id

--- pipeline/0x02-databases/8-genre_id_by_show.sql
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- query that shows the following:
 -- title, genre_id
-SELECT tv_shows.title, tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
     JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
