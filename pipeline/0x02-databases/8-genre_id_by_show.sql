5ed1301158d082963216f2411b838c62cda27bcb
edward0rtiz <931@holbertonschool.com>
2020-11-02 17:00:28 -0500

    [feature] add genre query

---
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- query that shows the following:
+-- title, genre_id
+SELECT tv_shows.title, tv_show_genres.show_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
