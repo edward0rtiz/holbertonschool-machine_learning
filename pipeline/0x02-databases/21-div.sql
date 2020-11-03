de1c1d4d006fa5af4a2d53473ac8cb379b32958a
edward0rtiz <931@holbertonschool.com>
2020-11-03 15:29:48 -0500

    [feature] add safediv function

---
+++ pipeline/0x02-databases/21-div.sql
@@ -0,0 +1,13 @@
+-- creates a function SafeDiv to divide and returns a number
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+    RETURNS FLOAT
+    BEGIN
+    DECLARE result FLOAT;
+        SET result = 0;
+        IF b <> 0 THEN
+            SET result = a / b;
+        END IF;
+        RETURN result;
+END $$
+DELIMITER ;
