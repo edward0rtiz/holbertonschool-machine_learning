a7a428d4f0e3b6bba38837d4d60610c104e5fd58
edward0rtiz <931@holbertonschool.com>
2020-11-02 22:45:26 -0500

    [feature] add new table

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- creates a tables users updated vars
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+    );
