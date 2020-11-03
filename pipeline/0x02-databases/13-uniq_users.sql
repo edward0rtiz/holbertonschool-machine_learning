fe55e49ed8bbbc09fe1a28db38221c6c593fba45
edward0rtiz <931@holbertonschool.com>
2020-11-02 22:37:33 -0500

    [feature] add new table fix mariadb dino conf

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create a table users
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
