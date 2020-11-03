5e919acb597e9aa8e2144d77f257760268c824b6
edward0rtiz <931@holbertonschool.com>
2020-11-02 20:37:23 -0500

    [feature] quarey using left join

---
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -0,0 +1,7 @@
+-- query to get all shows without genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+    ORDER BY tv_shows.title, tv_show_genres.genre_id;
